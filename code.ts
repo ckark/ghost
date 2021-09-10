232a6de585ca2786a71d836f9dc3fe69c6bbd10a
Chris Kark <ckark@alumni.stanford.edu>
2021-09-10 12:09:59 -0600

    Initial commit

---
+++ code.ts
@@ -0,0 +1,220 @@
+figma.parameters.on('input', (parameters: ParameterValues, currentKey: string, result: SuggestionResults) => {
+	const query = parameters[currentKey]
+	switch (currentKey) {
+		case 'color':
+			const colors = ['Gray', 'Black', 'White']
+			result.setSuggestions(colors.filter(s => s.includes(query)))
+			break
+		default:
+			return
+	}
+});
+figma.on('run', ({
+	parameters
+}: RunEvent) => {
+	0 === figma.currentPage.selection.length && (figma.notify("Select at least one item."), figma.closePlugin());
+	if (parameters) {
+		let color;
+		"Gray" === parameters.color && (color = {
+				r: .9,
+				g: .9,
+				b: .9
+			}),
+			"Black" === parameters.color && (color = {
+				r: 0,
+				g: 0,
+				b: 0
+			}),
+			"White" === parameters.color && (color = {
+				r: 1,
+				g: 1,
+				b: 1
+			});
+		let all = new Array();
+		const traversal = (e, f) => {
+			let count = 0;
+
+			function* read(nodes) {
+				const len = nodes.length;
+				if (len === 0) {
+					return;
+				}
+				for (let i = 0; i < len; i++) {
+					const node = nodes[i];
+					yield node;
+					let children = node.children;
+					if (children) {
+						yield* read(children);
+					}
+				}
+				f.push(nodes);
+			}
+			const it = read(e);
+			let res = it.next();
+			while (!res.done) {
+				count++;
+				res = it.next();
+			}
+		}
+		traversal(figma.currentPage.selection, all);
+		all = all.flat();
+		let instances = all.filter(n => n.type === 'INSTANCE').filter(n => n.id.substr(0, 1) !== "I") as InstanceNode[];
+		if (all.find(a => "INSTANCE" === a.type)) {
+			let a = new Array;
+			instances.forEach(l => a.push(l.detachInstance()));
+			let l = new Array;
+			traversal(a, l), (l = l.flat()).filter(a => "FRAME" === a.type).map(a => a.layoutMode = "NONE"), all = all.concat(l).flat()
+		}
+		all = all.filter(n => n.id.substr(0, 1) !== "I").filter(n => n.type !== "INSTANCE")
+		let frames = all.filter(n => n.type === "FRAME" && n.parent.type !== 'PAGE') as FrameNode[];
+		let shapes = all.filter(
+			n =>
+			n.type === 'ELLIPSE' ||
+			n.type === 'POLYGON' ||
+			n.type === 'RECTANGLE' ||
+			n.type === 'STAR'
+		) as SceneNode[];
+		let vectors = all.filter(n => n.type === 'VECTOR') as VectorNode[];
+		let text = all.filter(n => n.type === "TEXT") as TextNode[];
+		const ghostifyFrames = (n) => {
+			n.map(e => {
+				(e.effects = [{
+					type: "DROP_SHADOW",
+					color: {
+						r: 0,
+						g: 0,
+						b: 0,
+						a: 0
+					},
+					offset: {
+						x: 0,
+						y: 2
+					},
+					radius: 0,
+					spread: 0,
+					visible: true,
+					blendMode: "NORMAL",
+					showShadowBehindNode: true
+				}]),
+				(e.fills = [{
+					type: 'SOLID',
+					opacity: 0,
+					color: color,
+				}, ]);
+				e.strokeWeight = 0;
+				(e.strokes = [{
+					type: 'SOLID',
+					opacity: 0,
+					color: color,
+				}, ]);
+				e.strokeWeight = 0;
+			})
+		}
+		ghostifyFrames(frames);
+		const ghostifyVector = (n) => {
+			n.map((e) => {
+				e.resizeWithoutConstraints(e.width, e.height),
+					(e.x = e.relativeTransform[0][2]),
+					(e.y = e.relativeTransform[1][2]),
+					(e.fills = [{
+						type: 'SOLID',
+						color: color,
+					}, ]);
+				(e.strokes = [{
+					type: 'SOLID',
+					color: color,
+				}, ]);
+			});
+		}
+		ghostifyVector(vectors);
+		const ghostifyShapes = (n) => {
+			const nodes: SceneNode[] = [];
+			n.map((e) => {
+				const o = figma.createRectangle();
+				if (e.type === 'ELLIPSE') o.cornerRadius = 1000;
+				o.resizeWithoutConstraints(e.width, e.height),
+					(o.x = e.relativeTransform[0][2]),
+					(o.y = e.relativeTransform[1][2]),
+					(o.fills = [{
+						type: 'SOLID',
+						color: color,
+					}, ]);
+				nodes.push(o);
+				if (
+					e.parent.type === 'COMPONENT_SET' ||
+					e.parent.type === 'COMPONENT' ||
+					e.parent.type === 'PAGE'
+				) {
+					return;
+				} else {
+					e.parent.insertChild(e.parent.children.length, o), e.remove()
+				}
+			});
+		}
+		ghostifyShapes(shapes);
+		const ghostifyText = (n) => {
+			n.map((e) => {
+				let fontsize = Number(e.fontSize),
+					height = e.height,
+					lineHeight = e.lineHeight;
+				isNaN(lineHeight) && (lineHeight = 1.25 * fontsize);
+				const nodes: SceneNode[] = [],
+					numberOfRectangles = Math.round(height / lineHeight);
+				let index = n.indexOf(e);
+				for (let i = 0; i < numberOfRectangles; i++) {
+					const t = figma.createRectangle(),
+						o = (e, t) => Math.random() * (t - e) + e;
+					t.resizeWithoutConstraints((e.width, e.width), 0.7 * lineHeight),
+						(t.cornerRadius = lineHeight),
+						(t.x = e.relativeTransform[0][2]),
+						(t.y = e.relativeTransform[1][2] + lineHeight * i),
+						(t.fills = [{
+							type: 'SOLID',
+							color: color,
+						}, ]),
+						nodes.push(t),
+						e.parent.insertChild(e.parent.children.length, t);
+				}
+				e.remove();
+			});
+		};
+		ghostifyText(text);
+		const nest = () => {
+			const nest = Array.from(
+				figma.currentPage.findAll(
+					(item) => item.parent.type === 'PAGE',
+				) as SceneNode[],
+			);
+			const topframes = Array.from(
+				figma.currentPage.findAll(
+					(frame) => frame.type === 'FRAME' && frame.parent.type === 'PAGE',
+				) as FrameNode[],
+			);
+			frames.map(e => e.layoutMode = 'NONE')
+			0 != topframes.length &&
+				0 != nest.length &&
+				nest.map((a) => {
+					topframes.map((e) => {
+						if (!0 === overlap(a, e) && a.id != e.id) {
+							let t = Math.abs(e.x - a.x),
+								h = Math.abs(e.y - a.y);
+							e.appendChild(a), (a.x = t), (a.y = h);
+						}
+					});
+				});
+
+			function overlap(e, h) {
+				return (
+					e.parent.children.indexOf(e) > h.parent.children.indexOf(h) &&
+					e.x >= h.x &&
+					e.x + e.width <= h.x + h.width &&
+					e.y >= h.y &&
+					e.y + e.height <= h.y + h.height
+				);
+
+			}
+		};
+		nest();
+		figma.closePlugin('ghostified');
+	}
+});
