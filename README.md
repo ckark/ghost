1437787f0738a8f01f53e8696f2ff5c24fac7121
Chris Kark <ckark@alumni.stanford.edu>
2021-09-13 13:37:59 -0600

    production build

--- README.md
+++ README.md
@@ -1,22 +1,28 @@
 # Ghost
+
 Ghost is a lightweight plugin that converts high fidelity mockups to loading or “skeleton” screens.
 
 ## How it Works
+
 Ghost detects all text and shape layers (rectangles, ellipses, vectors, frames, etc.) selected. It then measures their dimensions before replacing them with rectangles; the original layers are removed.
 To general a layout faithful to the original, Ghost removes auto-layout, thereby preventing some of the anomalies that occur when inserting children into frames.
 
 ## Basic Usage
+
 1. Select a shape, text box, or an entire screen.
 2. Select a color.
 3. Ghost!
 
 ## Recommended Usage
+
 Loading is a state specific design akin to `disabled` or `destrutive`, `hover` or `active`. Create a ghostified loading variant.
 
 ## Roadmap
-- Color picker—depending on whether Figma’s new input parameters add support for HTML-like inputs like `input type=color`.
-- Linear gradients for a sheen effect.
-- Convert one or more directly selected instances.
+
+-   Color picker—depending on whether Figma’s new input parameters add support for HTML-like inputs like `input type=color`.
+-   Linear gradients for a sheen effect.
+-   Convert one or more directly selected instances.
 
 ## End Notes
+
 I’m relatively new to Typescript and working with Figma’s API in general. For questions and (constructive) feedback, send me a message.
--- code.ts
+++ code.ts
@@ -1,35 +1,36 @@
-figma.parameters.on('input', (parameters: ParameterValues, currentKey: string, result: SuggestionResults) => {
-	const query = parameters[currentKey]
+figma.parameters.on("input", (parameters: ParameterValues, currentKey: string, result: SuggestionResults) => {
+	const query = parameters[currentKey];
 	switch (currentKey) {
-		case 'color':
-			const colors = ['Gray', 'Black', 'White']
-			result.setSuggestions(colors.filter(s => s.includes(query)))
-			break
+		case "color":
+			const colors = ["Gray", "Black", "White"];
+			result.setSuggestions(colors.filter((s) => s.includes(query)));
+			break;
 		default:
-			return
+			return;
 	}
 });
-figma.on('run', ({
-	parameters
-}: RunEvent) => {
+figma.on("run", ({ parameters }: RunEvent) => {
 	0 === figma.currentPage.selection.length && (figma.notify("Select at least one item."), figma.closePlugin());
 	if (parameters) {
 		let color;
-		"Gray" === parameters.color && (color = {
-				r: .9,
-				g: .9,
-				b: .9
+		"Gray" === parameters.color &&
+			(color = {
+				r: 0.9,
+				g: 0.9,
+				b: 0.9,
 			}),
-			"Black" === parameters.color && (color = {
-				r: 0,
-				g: 0,
-				b: 0
-			}),
-			"White" === parameters.color && (color = {
-				r: 1,
-				g: 1,
-				b: 1
-			});
+			"Black" === parameters.color &&
+				(color = {
+					r: 0,
+					g: 0,
+					b: 0,
+				}),
+			"White" === parameters.color &&
+				(color = {
+					r: 1,
+					g: 1,
+					b: 1,
+				});
 		let all = new Array();
 		const traversal = (e, f) => {
 			let count = 0;
@@ -55,122 +56,172 @@ figma.on('run', ({
 				count++;
 				res = it.next();
 			}
-		}
+		};
 		traversal(figma.currentPage.selection, all);
 		all = all.flat();
-		const detach = t => {
-			let e = new Array;
-			if ((t = t.filter(t => "INSTANCE" === t.type).filter(t => "I" !== t.id.substr(0, 1))).length > 0)
-				return traversal(t.map(t => t.detachInstance()), e),
-					all.push(e.flat().filter(t => "INSTANCE" !== t.type).filter(t => "I" !== t.id.substr(0, 1))),
-					e = e.flat().filter(t => "INSTANCE" === t.type).filter(t => "I" !== t.id.substr(0, 1)),
+		const detach = (t) => {
+			let e = new Array();
+			if ((t = t.filter((t) => "INSTANCE" === t.type).filter((t) => "I" !== t.id.substr(0, 1))).length > 0)
+				return (
+					traversal(
+						t.map((t) => t.detachInstance()),
+						e
+					),
+					all.push(
+						e
+							.flat()
+							.filter((t) => "INSTANCE" !== t.type)
+							.filter((t) => "I" !== t.id.substr(0, 1))
+					),
+					(e = e
+						.flat()
+						.filter((t) => "INSTANCE" === t.type)
+						.filter((t) => "I" !== t.id.substr(0, 1))),
 					detach(e),
 					all.flat()
+				);
 		};
 		detach(all);
-		all = all.flat().filter(n => n.type !== 'INSTANCE').filter(n => n.id.substr(0, 1) !== "I");
-		let frames = all.filter(n => n.type === "FRAME" && n.parent.type !== 'PAGE');
-		let images = new Array,
-			nonimages = new Array;
-		frames.map(e => {
-			e.fills.map(a => {
+		all = all
+			.flat()
+			.filter((n) => n.type !== "INSTANCE")
+			.filter((n) => n.id.substr(0, 1) !== "I");
+		let frames = all.filter((n) => n.type === "FRAME" && n.parent.type !== "PAGE");
+		let images = new Array(),
+			nonimages = new Array();
+		frames.map((e) => {
+			e.fills.map((a) => {
 				a.type;
-				"IMAGE" === a.type ? images.push(e) : nonimages.push(e)
-			})
+				"IMAGE" === a.type ? images.push(e) : nonimages.push(e);
+			});
 		});
 		let shapes = all.filter(
-			n =>
-			n.type === 'ELLIPSE' ||
-			n.type === 'POLYGON' ||
-			n.type === 'RECTANGLE' ||
-			n.type === 'STAR'
+			(n) => n.type === "ELLIPSE" || n.type === "POLYGON" || n.type === "RECTANGLE" || n.type === "STAR"
 		) as SceneNode[];
-		let vectors = all.filter(n => n.type === 'VECTOR') as VectorNode[];
-		let text = all.filter(n => n.type === "TEXT") as TextNode[];
-		const ghostifyNonImages = o => {
-			o.map(o => {
-				o.effects = [{
-					type: "DROP_SHADOW",
-					color: {
-						r: 0,
-						g: 0,
-						b: 0,
-						a: 0
+		let vectors = all.filter((n) => n.type === "VECTOR") as VectorNode[];
+		let text = all.filter((n) => n.type === "TEXT") as TextNode[];
+		const ghostifyNonImages = (o) => {
+			o.map((o) => {
+				(o.effects = [
+					{
+						type: "DROP_SHADOW",
+						color: {
+							r: 0,
+							g: 0,
+							b: 0,
+							a: 0,
+						},
+						offset: {
+							x: 0,
+							y: 0,
+						},
+						radius: 0,
+						spread: 0,
+						visible: !0,
+						blendMode: "NORMAL",
+						showShadowBehindNode: !0,
 					},
-					offset: {
-						x: 0,
-						y: 0
-					},
-					radius: 0,
-					spread: 0,
-					visible: !0,
-					blendMode: "NORMAL",
-					showShadowBehindNode: !0
-				}], o.fills = [{
-					type: "SOLID",
-					opacity: 0,
-					color: color
-				}], o.strokeWeight = 0, o.strokes = [{
-					type: "SOLID",
-					opacity: 0,
-					color: color
-				}], o.strokeWeight = 0
-			})
+				]),
+					(o.fills = [
+						{
+							type: "SOLID",
+							opacity: 0,
+							color: color,
+						},
+					]),
+					(o.strokeWeight = 0),
+					(o.strokes = [
+						{
+							type: "SOLID",
+							opacity: 0,
+							color: color,
+						},
+					]),
+					(o.strokeWeight = 0);
+			});
 		};
 		ghostifyNonImages(nonimages);
-		const ghostifyImages = o => {
-			o.map(o => {
-				o.effects = [{
-					type: "DROP_SHADOW",
-					color: {
-						r: 0,
-						g: 0,
-						b: 0,
-						a: 0
+		const ghostifyImages = (o) => {
+			o.map((o) => {
+				(o.effects = [
+					{
+						type: "DROP_SHADOW",
+						color: {
+							r: 0,
+							g: 0,
+							b: 0,
+							a: 0,
+						},
+						offset: {
+							x: 0,
+							y: 0,
+						},
+						radius: 0,
+						spread: 0,
+						visible: !0,
+						blendMode: "NORMAL",
+						showShadowBehindNode: !0,
 					},
-					offset: {
-						x: 0,
-						y: 0
-					},
-					radius: 0,
-					spread: 0,
-					visible: !0,
-					blendMode: "NORMAL",
-					showShadowBehindNode: !0
-				}], o.fills = [{
-					type: "SOLID",
-					opacity: 1,
-					color: color
-				}], o.strokeWeight = 0, o.strokes = [{
-					type: "SOLID",
-					opacity: 0,
-					color: color
-				}], o.strokeWeight = 0
-			})
+				]),
+					(o.fills = [
+						{
+							type: "SOLID",
+							opacity: 1,
+							color: color,
+						},
+					]),
+					(o.strokeWeight = 0),
+					(o.strokes = [
+						{
+							type: "SOLID",
+							opacity: 0,
+							color: color,
+						},
+					]),
+					(o.strokeWeight = 0);
+			});
 		};
 		ghostifyImages(images);
-		const ghostifyVector = o => {
-			o.map(o => {
-				o.resizeWithoutConstraints(o.width, o.height), o.x = o.relativeTransform[0][2], o.y = o.relativeTransform[1][2], o.fills = [{
-					type: "SOLID",
-					color: color
-				}], o.strokes = [{
-					type: "SOLID",
-					color: color
-				}]
-			})
+		const ghostifyVector = (o) => {
+			o.map((o) => {
+				o.resizeWithoutConstraints(o.width, o.height),
+					(o.x = o.relativeTransform[0][2]),
+					(o.y = o.relativeTransform[1][2]),
+					(o.fills = [
+						{
+							type: "SOLID",
+							color: color,
+						},
+					]),
+					(o.strokes = [
+						{
+							type: "SOLID",
+							color: color,
+						},
+					]);
+			});
 		};
 		ghostifyVector(vectors);
 		const ghostifyShapes = (n) => {
 			const nodes: SceneNode[] = [];
-			n.map(e => {
+			n.map((e) => {
 				const t = figma.createRectangle();
-				"ELLIPSE" === e.type && (t.cornerRadius = 1e3), t.resizeWithoutConstraints(e.width, e.height), t.x = e.relativeTransform[0][2], t.y = e.relativeTransform[1][2], t.fills = [{
-					type: "SOLID",
-					color: color
-				}], nodes.push(t), "COMPONENT_SET" !== e.parent.type && "PAGE" !== e.parent.type && (e.parent.insertChild(e.parent.children.length, t), e.remove())
+				"ELLIPSE" === e.type && (t.cornerRadius = 1e3),
+					t.resizeWithoutConstraints(e.width, e.height),
+					(t.x = e.relativeTransform[0][2]),
+					(t.y = e.relativeTransform[1][2]),
+					(t.fills = [
+						{
+							type: "SOLID",
+							color: color,
+						},
+					]),
+					nodes.push(t),
+					"COMPONENT_SET" !== e.parent.type &&
+						"PAGE" !== e.parent.type &&
+						(e.parent.insertChild(e.parent.children.length, t), e.remove());
 			});
-		}
+		};
 		ghostifyShapes(shapes);
 		const ghostifyText = (n) => {
 			n.map((e) => {
@@ -182,27 +233,31 @@ figma.on('run', ({
 					numberOfRectangles = Math.round(height / lineHeight);
 				for (let t = 0; t < numberOfRectangles; t++) {
 					const i = figma.createRectangle();
-					i.resizeWithoutConstraints((e.width, e.width), .7 * lineHeight), i.cornerRadius = lineHeight, i.x = e.relativeTransform[0][2], i.y = e.relativeTransform[1][2] + lineHeight * t, i.fills = [{
-						type: "SOLID",
-						color: color
-					}], nodes.push(i), e.parent.insertChild(e.parent.children.length, i)
+					i.resizeWithoutConstraints((e.width, e.width), 0.7 * lineHeight),
+						(i.cornerRadius = lineHeight),
+						(i.x = e.relativeTransform[0][2]),
+						(i.y = e.relativeTransform[1][2] + lineHeight * t),
+						(i.fills = [
+							{
+								type: "SOLID",
+								color: color,
+							},
+						]),
+						nodes.push(i),
+						e.parent.insertChild(e.parent.children.length, i);
 				}
 				e.remove();
 			});
 		};
 		ghostifyText(text);
 		const nest = () => {
-			const nest = Array.from(
-				figma.currentPage.findAll(
-					(item) => item.parent.type === 'PAGE',
-				) as SceneNode[],
-			);
+			const nest = Array.from(figma.currentPage.findAll((item) => item.parent.type === "PAGE") as SceneNode[]);
 			const topframes = Array.from(
 				figma.currentPage.findAll(
-					(frame) => frame.type === 'FRAME' && frame.parent.type === 'PAGE',
-				) as FrameNode[],
+					(frame) => frame.type === "FRAME" && frame.parent.type === "PAGE"
+				) as FrameNode[]
 			);
-			frames.map(e => e.layoutMode = 'NONE');
+			frames.map((e) => (e.layoutMode = "NONE"));
 			0 != topframes.length &&
 				0 != nest.length &&
 				nest.map((a) => {
@@ -223,10 +278,9 @@ figma.on('run', ({
 					e.y >= h.y &&
 					e.y + e.height <= h.y + h.height
 				);
-
 			}
 		};
 		nest();
-		figma.closePlugin('ghostified');
+		figma.closePlugin("ghostified");
 	}
 });
