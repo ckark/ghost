6ed0aa73dacf4ea3f7da2ddbb59f054a779a3d85
Chris Kark <ckark@alumni.stanford.edu>
2021-11-27 08:00:23 -0700

    updated

--- code.js
+++ code.js
@@ -1,24 +1,26 @@
-let type = ["Solid", "Linear Gradient"], colors = ["Gray", "Black", "White"], fills;
-figma.parameters.on("input", ({ key, query, result }) => {
+let type = ['Solid', 'Linear Gradient'], colors = ['Gray', 'Black', 'White'], fills;
+figma.parameters.on('input', ({ key, query, result }) => {
     switch (key) {
-        case "type":
+        case 'type':
             result.setSuggestions(type.filter((s) => s.includes(query)));
             break;
-        case "color":
+        case 'color':
             result.setSuggestions(colors.filter((s) => s.includes(query)));
             break;
         default:
             return;
     }
 });
-figma.on("run", ({ parameters }) => {
-    0 === figma.currentPage.selection.length && (figma.notify("Select at least one item."), figma.closePlugin());
+figma.on('run', ({ parameters }) => {
+    0 === figma.currentPage.selection.length &&
+        (figma.notify('Select at least one item.'),
+            figma.closePlugin());
     parameters &&
-        "Gray" === parameters.color &&
-        "Solid" === parameters.type &&
+        'Gray' === parameters.color &&
+        'Solid' === parameters.type &&
         (fills = [
             {
-                type: "SOLID",
+                type: 'SOLID',
                 color: {
                     r: 0.9,
                     g: 0.9,
@@ -26,14 +28,18 @@ figma.on("run", ({ parameters }) => {
                 },
             },
         ]),
-        "Gray" === parameters.color &&
-            "Linear Gradient" === parameters.type &&
+        'Gray' === parameters.color &&
+            'Linear Gradient' === parameters.type &&
             (fills = [
                 {
-                    type: "GRADIENT_LINEAR",
+                    type: 'GRADIENT_LINEAR',
                     gradientTransform: [
                         [-1, 1.516437286852579e-8, 1],
-                        [-1.7966517162903983e-8, -0.0659240335226059, 0.5335403084754944],
+                        [
+                            -1.7966517162903983e-8,
+                            -0.0659240335226059,
+                            0.5335403084754944,
+                        ],
                     ],
                     gradientStops: [
                         {
@@ -57,11 +63,11 @@ figma.on("run", ({ parameters }) => {
                     ],
                 },
             ]),
-        "Black" === parameters.color &&
-            "Solid" === parameters.type &&
+        'Black' === parameters.color &&
+            'Solid' === parameters.type &&
             (fills = [
                 {
-                    type: "SOLID",
+                    type: 'SOLID',
                     color: {
                         r: 0,
                         g: 0,
@@ -69,14 +75,18 @@ figma.on("run", ({ parameters }) => {
                     },
                 },
             ]),
-        "Black" === parameters.color &&
-            "Linear Gradient" === parameters.type &&
+        'Black' === parameters.color &&
+            'Linear Gradient' === parameters.type &&
             (fills = [
                 {
-                    type: "GRADIENT_LINEAR",
+                    type: 'GRADIENT_LINEAR',
                     gradientTransform: [
                         [-1, 1.516437286852579e-8, 1],
-                        [-1.7966517162903983e-8, -0.0659240335226059, 0.5335403084754944],
+                        [
+                            -1.7966517162903983e-8,
+                            -0.0659240335226059,
+                            0.5335403084754944,
+                        ],
                     ],
                     gradientStops: [
                         {
@@ -100,11 +110,11 @@ figma.on("run", ({ parameters }) => {
                     ],
                 },
             ]),
-        "White" === parameters.color &&
-            "Solid" === parameters.type &&
+        'White' === parameters.color &&
+            'Solid' === parameters.type &&
             (fills = [
                 {
-                    type: "SOLID",
+                    type: 'SOLID',
                     color: {
                         r: 1,
                         g: 1,
@@ -112,14 +122,18 @@ figma.on("run", ({ parameters }) => {
                     },
                 },
             ]),
-        "White" === parameters.color &&
-            "Linear Gradient" === parameters.type &&
+        'White' === parameters.color &&
+            'Linear Gradient' === parameters.type &&
             (fills = [
                 {
-                    type: "GRADIENT_LINEAR",
+                    type: 'GRADIENT_LINEAR',
                     gradientTransform: [
                         [-1, 1.516437286852579e-8, 1],
-                        [-1.7966517162903983e-8, -0.0659240335226059, 0.5335403084754944],
+                        [
+                            -1.7966517162903983e-8,
+                            -0.0659240335226059,
+                            0.5335403084754944,
+                        ],
                     ],
                     gradientStops: [
                         {
@@ -171,40 +185,49 @@ figma.on("run", ({ parameters }) => {
     traversal(figma.currentPage.selection, all), (all = all.flat());
     const detach = (e) => {
         let t = new Array();
-        if ((e = e.filter((e) => "INSTANCE" === e.type).filter((e) => "I" !== e.id.substr(0, 1))).length > 0)
+        if ((e = e
+            .filter((e) => 'INSTANCE' === e.type)
+            .filter((e) => 'I' !== e.id.substr(0, 1)))
+            .length > 0)
             return (traversal(e.map((e) => e.detachInstance()), t),
                 all.push(t
                     .flat()
-                    .filter((e) => "INSTANCE" !== e.type)
-                    .filter((e) => "I" !== e.id.substr(0, 1))),
+                    .filter((e) => 'INSTANCE' !==
+                    e.type)
+                    .filter((e) => 'I' !==
+                    e.id.substr(0, 1))),
                 (t = t
                     .flat()
-                    .filter((e) => "INSTANCE" === e.type)
-                    .filter((e) => "I" !== e.id.substr(0, 1))),
+                    .filter((e) => 'INSTANCE' === e.type)
+                    .filter((e) => 'I' !==
+                    e.id.substr(0, 1))),
                 detach(t),
                 all.flat());
     };
     detach(all),
         (all = all
             .flat()
-            .filter((e) => "INSTANCE" !== e.type)
-            .filter((e) => "I" !== e.id.substr(0, 1)));
-    let frames = all.filter((e) => "FRAME" === e.type && "PAGE" !== e.parent.type), images = new Array(), nonimages = new Array();
+            .filter((e) => 'INSTANCE' !== e.type)
+            .filter((e) => 'I' !== e.id.substr(0, 1)));
+    let frames = all.filter((e) => 'FRAME' === e.type && 'PAGE' !== e.parent.type), images = new Array(), nonimages = new Array();
     frames.map((e) => {
         e.fills.map((t) => {
-            t.type, "IMAGE" === t.type ? images.push(e) : nonimages.push(e);
+            t.type,
+                'IMAGE' === t.type
+                    ? images.push(e)
+                    : nonimages.push(e);
         });
     }),
-        frames.map((e) => (e.layoutMode = "NONE"));
-    let shapes = all.filter((n) => n.type === "ELLIPSE" || n.type === "POLYGON" || n.type === "RECTANGLE" || n.type === "STAR"), vectors = all.filter((n) => n.type === "VECTOR"), text = all.filter((n) => n.type === "TEXT"), parentFrame = all.filter((e) => "FRAME" === e.type && "PAGE" === e.parent.type), nonFrames = new Array();
-    nonFrames.map(e => e.push(all.filter((n) => n.type !== "FRAME")));
-    parentFrame.map((e) => { e.appendChild(nonFrames.flat()); });
-    frames.map((e) => { e.remove(); });
+        frames.map((e) => (e.layoutMode = 'NONE'));
+    let shapes = all.filter((n) => n.type === 'ELLIPSE' ||
+        n.type === 'POLYGON' ||
+        n.type === 'RECTANGLE' ||
+        n.type === 'STAR'), vectors = all.filter((n) => n.type === 'VECTOR'), text = all.filter((n) => n.type === 'TEXT');
     const nodes = [], ghostifyNonImages = (e) => {
         e.map((e) => {
             (e.effects = [
                 {
-                    type: "DROP_SHADOW",
+                    type: 'DROP_SHADOW',
                     color: {
                         r: 0,
                         g: 0,
@@ -218,13 +241,13 @@ figma.on("run", ({ parameters }) => {
                     radius: 0,
                     spread: 0,
                     visible: !0,
-                    blendMode: "NORMAL",
+                    blendMode: 'NORMAL',
                     showShadowBehindNode: !0,
                 },
             ]),
                 (e.fills = [
                     {
-                        type: "SOLID",
+                        type: 'SOLID',
                         opacity: 0,
                         color: {
                             r: 0,
@@ -236,7 +259,7 @@ figma.on("run", ({ parameters }) => {
                 (e.strokeWeight = 0),
                 (e.strokes = [
                     {
-                        type: "SOLID",
+                        type: 'SOLID',
                         opacity: 0,
                         color: {
                             r: 0,
@@ -250,7 +273,7 @@ figma.on("run", ({ parameters }) => {
         e.map((e) => {
             (e.effects = [
                 {
-                    type: "DROP_SHADOW",
+                    type: 'DROP_SHADOW',
                     color: {
                         r: 0,
                         g: 0,
@@ -264,16 +287,17 @@ figma.on("run", ({ parameters }) => {
                     radius: 0,
                     spread: 0,
                     visible: !0,
-                    blendMode: "NORMAL",
+                    blendMode: 'NORMAL',
                     showShadowBehindNode: !0,
                 },
             ]),
                 (e.fills = fills),
-                e.strokeWeight > 0 && (e.strokes = fills),
+                e.strokeWeight > 0 &&
+                    (e.strokes = fills),
                 0 === e.strokeWeight &&
                     (e.strokes = [
                         {
-                            type: "SOLID",
+                            type: 'SOLID',
                             opacity: 0,
                             color: {
                                 r: 0,
@@ -287,11 +311,12 @@ figma.on("run", ({ parameters }) => {
     }, ghostifyVector = (e) => {
         e.map((e) => {
             (e.fills = fills),
-                e.strokeWeight > 0 && (e.strokes = fills),
+                e.strokeWeight > 0 &&
+                    (e.strokes = fills),
                 0 === e.strokeWeight &&
                     (e.strokes = [
                         {
-                            type: "SOLID",
+                            type: 'SOLID',
                             opacity: 0,
                             color: {
                                 r: 0,
@@ -305,17 +330,20 @@ figma.on("run", ({ parameters }) => {
     }, ghostifyShapes = (e) => {
         e.map((e) => {
             const s = figma.createRectangle();
-            0 !== e.cornerRadius && (s.cornerRadius = e.width),
-                "ELLIPSE" === e.type && (s.cornerRadius = e.width),
+            0 !== e.cornerRadius &&
+                (s.cornerRadius = e.width),
+                'ELLIPSE' === e.type &&
+                    (s.cornerRadius = e.width),
                 s.resizeWithoutConstraints(e.width, e.height),
                 (s.x = e.relativeTransform[0][2]),
                 (s.y = e.relativeTransform[1][2]),
                 (s.fills = fills),
-                e.strokeWeight > 0 && (s.strokes = fills),
+                e.strokeWeight > 0 &&
+                    (s.strokes = fills),
                 0 === e.strokeWeight &&
                     (s.strokes = [
                         {
-                            type: "SOLID",
+                            type: 'SOLID',
                             opacity: 0,
                             color: {
                                 r: 0,
@@ -325,32 +353,40 @@ figma.on("run", ({ parameters }) => {
                         },
                     ]),
                 nodes.push(s),
-                "COMPONENT_SET" !== e.parent.type &&
-                    "PAGE" !== e.parent.type &&
-                    (e.parent.insertChild(e.parent.children.length, s), e.remove());
+                'COMPONENT_SET' !== e.parent.type &&
+                    'PAGE' !== e.parent.type &&
+                    (e.parent.insertChild(e.parent.children
+                        .length, s),
+                        e.remove());
         });
     }, ghostifyText = (e) => new Promise((t) => {
         e.map(async (e) => {
             await figma.loadFontAsync(e.fontName);
-            (e.textAutoResize = "WIDTH_AND_HEIGHT"),
+            (e.textAutoResize = 'WIDTH_AND_HEIGHT'),
                 !0 === e.hasMissingFont &&
-                    figma.closePlugin("You can't convert text until loading the font on which it's based");
+                    figma.closePlugin("You can't convert text until loading its source font.");
             let t = Number(e.fontSize), i = e.height, s = e.lineHeight;
             isNaN(s) && (s = 1.25 * t);
             const o = Math.round(i / s);
             for (let t = 0; t < o; t++) {
                 const i = figma.createRectangle();
-                i.resizeWithoutConstraints((e.width, e.width), 0.7 * s),
+                i.resizeWithoutConstraints(e.width, (e.height, 0.7 * s)),
                     (i.cornerRadius = s),
-                    (i.x = e.relativeTransform[0][2]),
-                    (i.y = e.relativeTransform[1][2] + s * t),
+                    (i.x =
+                        e.relativeTransform[0][2]),
+                    (i.y =
+                        e
+                            .relativeTransform[1][2] +
+                            s * t),
                     (i.fills = fills),
                     nodes.push(i),
-                    e.parent.insertChild(e.parent.children.length, i),
-                    e.remove();
+                    e.parent.insertChild(e.parent
+                        .children
+                        .length, i);
             }
+            e.remove();
         }),
-            setTimeout(() => t("done"), 0);
+            setTimeout(() => t('done'), 0);
     }), ghostify = async () => {
         ghostifyNonImages(nonimages),
             ghostifyImages(images),
@@ -358,7 +394,7 @@ figma.on("run", ({ parameters }) => {
             ghostifyShapes(shapes),
             await ghostifyText(text),
             console.clear,
-            figma.closePlugin("ghostified");
+            figma.closePlugin('ghostified');
     };
     ghostify();
 });
--- code.ts
+++ code.ts
@@ -1,26 +1,32 @@
-let type = ["Solid", "Linear Gradient"],
-	colors = ["Gray", "Black", "White"],
+let type = ['Solid', 'Linear Gradient'],
+	colors = ['Gray', 'Black', 'White'],
 	fills;
-figma.parameters.on("input", ({ key, query, result }: ParameterInputEvent) => {
+figma.parameters.on('input', ({ key, query, result }: ParameterInputEvent) => {
 	switch (key) {
-		case "type":
-			result.setSuggestions(type.filter((s) => s.includes(query)));
+		case 'type':
+			result.setSuggestions(
+				type.filter((s) => s.includes(query)),
+			);
 			break;
-		case "color":
-			result.setSuggestions(colors.filter((s) => s.includes(query)));
+		case 'color':
+			result.setSuggestions(
+				colors.filter((s) => s.includes(query)),
+			);
 			break;
 		default:
 			return;
 	}
 });
-figma.on("run", ({ parameters }: RunEvent) => {
-	0 === figma.currentPage.selection.length && (figma.notify("Select at least one item."), figma.closePlugin());
+figma.on('run', ({ parameters }: RunEvent) => {
+	0 === figma.currentPage.selection.length &&
+		(figma.notify('Select at least one item.'),
+		figma.closePlugin());
 	parameters &&
-		"Gray" === parameters.color &&
-		"Solid" === parameters.type &&
+		'Gray' === parameters.color &&
+		'Solid' === parameters.type &&
 		(fills = [
 			{
-				type: "SOLID",
+				type: 'SOLID',
 				color: {
 					r: 0.9,
 					g: 0.9,
@@ -28,14 +34,18 @@ figma.on("run", ({ parameters }: RunEvent) => {
 				},
 			},
 		]),
-		"Gray" === parameters.color &&
-			"Linear Gradient" === parameters.type &&
+		'Gray' === parameters.color &&
+			'Linear Gradient' === parameters.type &&
 			(fills = [
 				{
-					type: "GRADIENT_LINEAR",
+					type: 'GRADIENT_LINEAR',
 					gradientTransform: [
 						[-1, 1.516437286852579e-8, 1],
-						[-1.7966517162903983e-8, -0.0659240335226059, 0.5335403084754944],
+						[
+							-1.7966517162903983e-8,
+							-0.0659240335226059,
+							0.5335403084754944,
+						],
 					],
 					gradientStops: [
 						{
@@ -59,11 +69,11 @@ figma.on("run", ({ parameters }: RunEvent) => {
 					],
 				},
 			]),
-		"Black" === parameters.color &&
-			"Solid" === parameters.type &&
+		'Black' === parameters.color &&
+			'Solid' === parameters.type &&
 			(fills = [
 				{
-					type: "SOLID",
+					type: 'SOLID',
 					color: {
 						r: 0,
 						g: 0,
@@ -71,14 +81,18 @@ figma.on("run", ({ parameters }: RunEvent) => {
 					},
 				},
 			]),
-		"Black" === parameters.color &&
-			"Linear Gradient" === parameters.type &&
+		'Black' === parameters.color &&
+			'Linear Gradient' === parameters.type &&
 			(fills = [
 				{
-					type: "GRADIENT_LINEAR",
+					type: 'GRADIENT_LINEAR',
 					gradientTransform: [
 						[-1, 1.516437286852579e-8, 1],
-						[-1.7966517162903983e-8, -0.0659240335226059, 0.5335403084754944],
+						[
+							-1.7966517162903983e-8,
+							-0.0659240335226059,
+							0.5335403084754944,
+						],
 					],
 					gradientStops: [
 						{
@@ -102,11 +116,11 @@ figma.on("run", ({ parameters }: RunEvent) => {
 					],
 				},
 			]),
-		"White" === parameters.color &&
-			"Solid" === parameters.type &&
+		'White' === parameters.color &&
+			'Solid' === parameters.type &&
 			(fills = [
 				{
-					type: "SOLID",
+					type: 'SOLID',
 					color: {
 						r: 1,
 						g: 1,
@@ -114,14 +128,18 @@ figma.on("run", ({ parameters }: RunEvent) => {
 					},
 				},
 			]),
-		"White" === parameters.color &&
-			"Linear Gradient" === parameters.type &&
+		'White' === parameters.color &&
+			'Linear Gradient' === parameters.type &&
 			(fills = [
 				{
-					type: "GRADIENT_LINEAR",
+					type: 'GRADIENT_LINEAR',
 					gradientTransform: [
 						[-1, 1.516437286852579e-8, 1],
-						[-1.7966517162903983e-8, -0.0659240335226059, 0.5335403084754944],
+						[
+							-1.7966517162903983e-8,
+							-0.0659240335226059,
+							0.5335403084754944,
+						],
 					],
 					gradientStops: [
 						{
@@ -174,22 +192,42 @@ figma.on("run", ({ parameters }: RunEvent) => {
 	traversal(figma.currentPage.selection, all), (all = all.flat());
 	const detach = (e) => {
 		let t = new Array();
-		if ((e = e.filter((e) => "INSTANCE" === e.type).filter((e) => "I" !== e.id.substr(0, 1))).length > 0)
+		if (
+			(e = e
+				.filter((e) => 'INSTANCE' === e.type)
+				.filter((e) => 'I' !== e.id.substr(0, 1)))
+				.length > 0
+		)
 			return (
 				traversal(
 					e.map((e) => e.detachInstance()),
-					t
+					t,
 				),
 				all.push(
 					t
 						.flat()
-						.filter((e) => "INSTANCE" !== e.type)
-						.filter((e) => "I" !== e.id.substr(0, 1))
+						.filter(
+							(e) =>
+								'INSTANCE' !==
+								e.type,
+						)
+						.filter(
+							(e) =>
+								'I' !==
+								e.id.substr(
+									0,
+									1,
+								),
+						),
 				),
 				(t = t
 					.flat()
-					.filter((e) => "INSTANCE" === e.type)
-					.filter((e) => "I" !== e.id.substr(0, 1))),
+					.filter((e) => 'INSTANCE' === e.type)
+					.filter(
+						(e) =>
+							'I' !==
+							e.id.substr(0, 1),
+					)),
 				detach(t),
 				all.flat()
 			);
@@ -197,33 +235,39 @@ figma.on("run", ({ parameters }: RunEvent) => {
 	detach(all),
 		(all = all
 			.flat()
-			.filter((e) => "INSTANCE" !== e.type)
-			.filter((e) => "I" !== e.id.substr(0, 1)));
-	let frames = all.filter((e) => "FRAME" === e.type && "PAGE" !== e.parent.type),
+			.filter((e) => 'INSTANCE' !== e.type)
+			.filter((e) => 'I' !== e.id.substr(0, 1)));
+	let frames = all.filter(
+			(e) => 'FRAME' === e.type && 'PAGE' !== e.parent.type,
+		),
 		images = new Array(),
 		nonimages = new Array();
 	frames.map((e) => {
 		e.fills.map((t) => {
-			t.type, "IMAGE" === t.type ? images.push(e) : nonimages.push(e);
+			t.type,
+				'IMAGE' === t.type
+					? images.push(e)
+					: nonimages.push(e);
 		});
 	}),
-		frames.map((e) => (e.layoutMode = "NONE"));
+		frames.map((e) => (e.layoutMode = 'NONE'));
 	let shapes = all.filter(
-			(n) => n.type === "ELLIPSE" || n.type === "POLYGON" || n.type === "RECTANGLE" || n.type === "STAR"
+			(n) =>
+				n.type === 'ELLIPSE' ||
+				n.type === 'POLYGON' ||
+				n.type === 'RECTANGLE' ||
+				n.type === 'STAR',
 		) as SceneNode[],
-		vectors = all.filter((n) => n.type === "VECTOR") as VectorNode[],
-		text = all.filter((n) => n.type === "TEXT") as TextNode[],
-		parentFrame = all.filter((e) => "FRAME" === e.type && "PAGE" === e.parent.type),
-		nonFrames = new Array();
-		nonFrames.map(e=> e.push(all.filter((n) => n.type !== "FRAME")));
-		parentFrame.map((e) => {e.appendChild(nonFrames.flat())});
-		frames.map((e) => {e.remove()});
+		vectors = all.filter(
+			(n) => n.type === 'VECTOR',
+		) as VectorNode[],
+		text = all.filter((n) => n.type === 'TEXT') as TextNode[];
 	const nodes: SceneNode[] = [],
 		ghostifyNonImages = (e) => {
 			e.map((e) => {
 				(e.effects = [
 					{
-						type: "DROP_SHADOW",
+						type: 'DROP_SHADOW',
 						color: {
 							r: 0,
 							g: 0,
@@ -237,13 +281,13 @@ figma.on("run", ({ parameters }: RunEvent) => {
 						radius: 0,
 						spread: 0,
 						visible: !0,
-						blendMode: "NORMAL",
+						blendMode: 'NORMAL',
 						showShadowBehindNode: !0,
 					},
 				]),
 					(e.fills = [
 						{
-							type: "SOLID",
+							type: 'SOLID',
 							opacity: 0,
 							color: {
 								r: 0,
@@ -255,7 +299,7 @@ figma.on("run", ({ parameters }: RunEvent) => {
 					(e.strokeWeight = 0),
 					(e.strokes = [
 						{
-							type: "SOLID",
+							type: 'SOLID',
 							opacity: 0,
 							color: {
 								r: 0,
@@ -270,7 +314,7 @@ figma.on("run", ({ parameters }: RunEvent) => {
 			e.map((e) => {
 				(e.effects = [
 					{
-						type: "DROP_SHADOW",
+						type: 'DROP_SHADOW',
 						color: {
 							r: 0,
 							g: 0,
@@ -284,16 +328,17 @@ figma.on("run", ({ parameters }: RunEvent) => {
 						radius: 0,
 						spread: 0,
 						visible: !0,
-						blendMode: "NORMAL",
+						blendMode: 'NORMAL',
 						showShadowBehindNode: !0,
 					},
 				]),
 					(e.fills = fills),
-					e.strokeWeight > 0 && (e.strokes = fills),
+					e.strokeWeight > 0 &&
+						(e.strokes = fills),
 					0 === e.strokeWeight &&
 						(e.strokes = [
 							{
-								type: "SOLID",
+								type: 'SOLID',
 								opacity: 0,
 								color: {
 									r: 0,
@@ -308,11 +353,12 @@ figma.on("run", ({ parameters }: RunEvent) => {
 		ghostifyVector = (e) => {
 			e.map((e) => {
 				(e.fills = fills),
-					e.strokeWeight > 0 && (e.strokes = fills),
+					e.strokeWeight > 0 &&
+						(e.strokes = fills),
 					0 === e.strokeWeight &&
 						(e.strokes = [
 							{
-								type: "SOLID",
+								type: 'SOLID',
 								opacity: 0,
 								color: {
 									r: 0,
@@ -327,17 +373,23 @@ figma.on("run", ({ parameters }: RunEvent) => {
 		ghostifyShapes = (e) => {
 			e.map((e) => {
 				const s = figma.createRectangle();
-				0 !== e.cornerRadius && (s.cornerRadius = e.width),
-					"ELLIPSE" === e.type && (s.cornerRadius = e.width),
-					s.resizeWithoutConstraints(e.width, e.height),
+				0 !== e.cornerRadius &&
+					(s.cornerRadius = e.width),
+					'ELLIPSE' === e.type &&
+						(s.cornerRadius = e.width),
+					s.resizeWithoutConstraints(
+						e.width,
+						e.height,
+					),
 					(s.x = e.relativeTransform[0][2]),
 					(s.y = e.relativeTransform[1][2]),
 					(s.fills = fills),
-					e.strokeWeight > 0 && (s.strokes = fills),
+					e.strokeWeight > 0 &&
+						(s.strokes = fills),
 					0 === e.strokeWeight &&
 						(s.strokes = [
 							{
-								type: "SOLID",
+								type: 'SOLID',
 								opacity: 0,
 								color: {
 									r: 0,
@@ -347,36 +399,58 @@ figma.on("run", ({ parameters }: RunEvent) => {
 							},
 						]),
 					nodes.push(s),
-					"COMPONENT_SET" !== e.parent.type &&
-						"PAGE" !== e.parent.type &&
-						(e.parent.insertChild(e.parent.children.length, s), e.remove());
+					'COMPONENT_SET' !== e.parent.type &&
+						'PAGE' !== e.parent.type &&
+						(e.parent.insertChild(
+							e.parent.children
+								.length,
+							s,
+						),
+						e.remove());
 			});
 		},
 		ghostifyText = (e) =>
 			new Promise((t) => {
 				e.map(async (e) => {
-					await figma.loadFontAsync(e.fontName as FontName);
-					(e.textAutoResize = "WIDTH_AND_HEIGHT"),
+					await figma.loadFontAsync(
+						e.fontName as FontName,
+					);
+					(e.textAutoResize = 'WIDTH_AND_HEIGHT'),
 						!0 === e.hasMissingFont &&
-							figma.closePlugin("You can't convert text until loading the font on which it's based");
+							figma.closePlugin(
+								"You can't convert text until loading its source font.",
+							);
 					let t = Number(e.fontSize),
 						i = e.height,
 						s = e.lineHeight;
 					isNaN(s) && (s = 1.25 * t);
 					const o = Math.round(i / s);
 					for (let t = 0; t < o; t++) {
-						const i = figma.createRectangle();
-						i.resizeWithoutConstraints((e.width, e.width), 0.7 * s),
+						const i =
+							figma.createRectangle();
+						i.resizeWithoutConstraints(
+							e.width,
+							(e.height, 0.7 * s),
+						),
 							(i.cornerRadius = s),
-							(i.x = e.relativeTransform[0][2]),
-							(i.y = e.relativeTransform[1][2] + s * t),
+							(i.x =
+								e.relativeTransform[0][2]),
+							(i.y =
+								e
+									.relativeTransform[1][2] +
+								s * t),
 							(i.fills = fills),
 							nodes.push(i),
-							e.parent.insertChild(e.parent.children.length, i),
-							e.remove();
+							e.parent.insertChild(
+								e.parent
+									.children
+									.length,
+								i,
+							);
 					}
+					e.remove();
 				}),
-					setTimeout(() => t("done"), 0);
+					setTimeout(() => t('done'), 0);
 			}),
 		ghostify = async () => {
 			ghostifyNonImages(nonimages),
@@ -385,7 +459,7 @@ figma.on("run", ({ parameters }: RunEvent) => {
 				ghostifyShapes(shapes),
 				await ghostifyText(text),
 				console.clear,
-				figma.closePlugin("ghostified");
+				figma.closePlugin('ghostified');
 		};
 	ghostify();
 });
